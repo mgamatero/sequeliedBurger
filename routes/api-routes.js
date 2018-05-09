839d85f6c3d9e82381a6d04ba8f6f818ed61bb08
mgamatero <mgamatero@yahoo.com>
2018-05-09 13:43:57 -0700

    Added comment - Devour button doesnt work

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,20 +1,28 @@
-var db = require ('../models')
+var db = require('../models')
 
- module.exports = function(app){
-app.get('/api/burgers',function(req,res){
-    db.burger.findAll({}).then(function(r){
-        return res.json(r)
-        // console.log(r)
+module.exports = function (app) {
+    app.get('/api/burgers', function (req, res) {
+        db.burger.findAll({}).then(function (r) {
+            res.json(r)
+            // console.log(r)
+        })
     })
-})
 
-app.post('/api/burgers/:name', function(req,res){
-    db.burger.create({
-        name:req.params.name,
-        eaten:"N"
-    }).then(function(r) {
-        // console.log(res.json(r))
-        console.log(req.params.name + "added")
+    app.post('/api/burgers/:name', function (req, res) {
+        db.burger.create({
+            name: req.params.name,
+            eaten: "N"
+        }).then(function (r) {
+            res.json(r)
+            console.log(req.params.name + "added")
+        })
+    }
+    )
+    app.delete('/api/burgers/:name', function (req, res) {
+        db.burger.create({
+            id: req.params.id
+        }).then(function (r) {
+           
+        })
     })
-})
 }
