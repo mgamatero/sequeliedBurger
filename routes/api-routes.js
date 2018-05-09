7d0bc076bea6938bd40e5075ca88e08f8cb85678
mgamatero <mgamatero@yahoo.com>
2018-05-09 13:35:30 -0700

    Able to add to DB.  Appears on initial run and after each add click

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,16 +4,17 @@ var db = require ('../models')
 app.get('/api/burgers',function(req,res){
     db.burger.findAll({}).then(function(r){
         return res.json(r)
-        console.log(r)
+        // console.log(r)
     })
 })
 
-app.post('/api/burgers', function(req,res){
+app.post('/api/burgers/:name', function(req,res){
     db.burger.create({
-        name:"Big Mac",
+        name:req.params.name,
         eaten:"N"
     }).then(function(r) {
-        console.log(res.json(r))
+        // console.log(res.json(r))
+        console.log(req.params.name + "added")
     })
 })
 }
